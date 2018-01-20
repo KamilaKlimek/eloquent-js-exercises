9ab00828e24fca3c68ca0a65753e8e977e5bb75d
Kamila Klimek <32499565+KamilaKlimek@users.noreply.github.com>
2018-01-19 19:02:38 -0800

    Update sum-of-a-range.js

--- chapter4/sum-of-a-range.js
+++ chapter4/sum-of-a-range.js
@@ -14,13 +14,68 @@ function ranged(start, end) {
 }
 console.log(ranged(0,5))
 
-// 2. Sum function that takes the array and adds numbers inside
+// 2. Sum function that takes the array and adds all numbers inside
+
+function reductor(total, current) {
+  return total + current
+}
 
 function sumArrayItem(array) {
-  var total = 0
-  for (var i = 0; i < array.length; i++) {
-    total += array[i];
-  }
-  return total
+  var sum = array.reduce(reductor);
+  return sum;
 }
+
+console.log(sumArrayItem(rangedArray));
+
+//3. Take an optional argument "step" for the count; default step = 1 (full function below; sumArrayItem function repeated).
+
+var rangedArray = [];
+
+function ranged(start, end, step = 1) {
+
+  for (var i = start; i <= end; i+=step) {
+    rangedArray.push(i);
+  }
+  return rangedArray
+}
+
+console.log(ranged(1, 10, 2))
+
+function reductor(total, current) {
+  return total + current
+}
+
+function sumArrayItem(array) {
+  var sum = array.reduce(reductor);
+  return sum;
+}
+
+console.log(sumArrayItem(rangedArray));
+
+//4. Make sure it handles decreasing range (i.e. (5, 2, -1)).
+
+var rangedArray = [];
+
+function ranged(start, end, step = 1) {
+
+  for (var i = start; i <= end; i+=step) {
+    rangedArray.push(i);
+  }
+  for (var i = start; i >= end; i+=step) {
+    rangedArray.push(i);
+  }
+  return rangedArray
+}
+
+console.log(ranged(10, 2, -2))
+
+function reductor(total, current) {
+  return total + current
+}
+
+function sumArrayItem(array) {
+  var sum = array.reduce(reductor);
+  return sum;
+}
+
 console.log(sumArrayItem(rangedArray));
