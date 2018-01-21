2d07b5e7db5e84570cad1b7f1fb236fffb879ff6
Kamila Klimek <32499565+KamilaKlimek@users.noreply.github.com>
2018-01-20 17:24:59 -0800

    Update a-list.js

--- chapter4/a-list.js
+++ chapter4/a-list.js
@@ -12,8 +12,29 @@ function arrayToList(array) {
   }
   return list
 }
-
 console.log(arrayToList(arr))
-
+var myList = (arrayToList(arr))  //I use myList later
 //2. ListToArray
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest) {
+  array.push(node.value);
+  }
+  return array;
+}
+console.log(listToArray({ value: 10, rest: { value: 20, rest: null } }))
+
+//3. Prepend function to add element to the front of the list
+function prepend(element, list) {
+  var newList = {value: element, rest:list||null}
+  return newList               
+}
+console.log(prepend(5, myList))
+//4. Function nth that returns element at a number position in the list
+
+function nth(list, number) {
+  return list[Object.keys(list)[number]]
+}
+console.log(nth(myList, 4))
 
