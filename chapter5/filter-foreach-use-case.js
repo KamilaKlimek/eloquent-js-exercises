261d0b8109fcb42fcc7003f1c9401918e0481d66
Kamila Klimek <32499565+KamilaKlimek@users.noreply.github.com>
2018-01-23 11:40:31 -0800

    Update filter-foreach-use-case.js

--- chapter5/filter-foreach-use-case.js
+++ chapter5/filter-foreach-use-case.js
@@ -10,20 +10,20 @@ function filter(array, test) {
   }
 return passed;
 }
-
 console.log(filter(ancestry, function(person) {
   return person.born > 1900 && person.born < 1925
 }))
+
+
 //Fetching young people using a for loop.
 function filter(array, test) {
   var passed = [];
-  ancestry.forEach(function (member) {
+  array.forEach(function (member) {
     if (test(member))
       passed.push(member);
   });
 return passed;
 }
-
 console.log(filter(ancestry, function(person) {
   return person.born > 1900 && person.born < 1925
 }))
